1bff49498d41500de17a924d458666449d2472b5
Anttonilv <antton00@outlook.com>
2025-12-14 18:09:22 +0200

    valmis

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",  // käytä salasanaa jos sinulla on sellainen
+    password: "root", 
     database: "booksdb"
 });
 
